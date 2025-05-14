771440eed88144c07eca2c63155929851d4938d1
MMKT1977 <kennedy.moranga@student.moringaschool..com>
2025-05-14 12:51:47 +0300

    Review model created and seeded db

--- server/models.py
+++ server/models.py
@@ -17,6 +17,10 @@ class Customer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
 
+    reviews= db.relationship('Review', back_populates='customer')
+
+    items = association_proxy('reviews', 'item', creator=lambda item_obj: Review(item=item_obj))
+
     def __repr__(self):
         return f'<Customer {self.id}, {self.name}>'
 
@@ -28,5 +32,26 @@ class Item(db.Model):
     name = db.Column(db.String)
     price = db.Column(db.Float)
 
+    reviews = db.relationship('Review', back_populates='item')
+    
+    customers = association_proxy('reviews', 'customer',creator=lambda customer_obj: Review(customer=customer_obj))
+
     def __repr__(self):
         return f'<Item {self.id}, {self.name}, {self.price}>'
+
+class Review(db.Model):
+    __tablename__ = 'reviews'
+    id = db.Column(db.Integer, primary_key=True)
+    comment = db.Column(db.String)
+
+    #foreign keys
+    customer_id = db.Column(db.Integer, db.ForeignKey('customers.id'))
+    item_id = db.Column(db.Integer, db.ForeignKey('items.id'))
+
+    #relationships
+    customer = db.relationship('Customer', back_populates ='reviews')
+    item = db.relationship('Item', back_populates = 'reviews')
+
+    def __repr__(self):
+        return f'<Review {self.id}, {self.comment}, Customer: {self.customer_id}, Item: {self.item_id}>'
+
